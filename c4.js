0894d76a911ad4e03420ec6a6d52512c5bbfc9c7
Joe Kuang <four@berkeley.edu>
2016-08-09 11:45:15 -0700

    Refactoring

--- c4.js
+++ c4.js
@@ -7,39 +7,26 @@ var c4 = require("./game");
 var clients = [];
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/c4.html');
+    res.sendFile(__dirname + '/c4.html');
 });
 
 io.on("connection", function(socket) {
-
-    // Push user socket id to CLIENTS
     console.log("A user has connected.")
     clients.push(socket.id);
     console.log(clients);
 
-    // Attempt to start a new game
-    startNewGame();
-    // Display
+    startGame();
     display();
     
     socket.on("disconnect", function() {
-        // On disconnect, remove socket id from CLIENTS
         console.log("A user has disconnected");
         var index = clients.indexOf(socket.id);
         clients.splice(index, 1);
+        console.log(clients);
         
-        // If a ongoing player disconnects.
-        if (c4.ongoing && index < 2) {
-            io.emit('exit');
-            c4.ongoing = false;
-            // Move previous player to end of line.
-            var prev = clients.shift();
-            if (prev) {
-                clients.push(prev);
-            }
-            // Start a new game with existing clients.
-            setTimeout(startNewGame, 10000);
-            display();
+        if (c4.inGame() && index < 2) {
+            c4.endGame();
+            restartGame("A player has left.");
         }
     });
 
@@ -51,83 +38,87 @@ io.on("connection", function(socket) {
                 c4.placePiece(col);
                 display();
                 if (c4.isWon()) {
-                    c4.ongoing = false;
-                    var players = ['Red', 'Yellow'];
-                    io.emit('won', players[c4.whoWon()]);
-                    var prev = clients.shift();
-                    if (prev) {
-                        clients.push(prev);
-                    }
-                    setTimeout(startNewGame, 10000);
+                    endGame();
+                    restartGame();
                 } else if (c4.boardFull()) {
-                    c4.ongoing = false;
-                    io.emit('won', 'Neither player');
-                    var prev = clients.shift();
-                    if (prev) {
-                        clients.push(prev);
-                    }
-                    setTimeout(startNewGame, 10000);
+                    endGame();
+                    restartGame('Neither player wins.');
                 }
             }
         }
     });
 
-
 });
 
 http.listen(process.env.PORT || 3000, function(){
-  console.log('Starting C4');
-  console.log(__dirname);
+    console.log('Starting C4');
+    console.log(__dirname);
 });
 
-function startNewGame() {
-    if (!c4.ongoing) {
-        c4.newGame();
+function startGame() {
+    if (clients.length >= 2 && !c4.inGame() && !c4.isWon()) {
         clearDisplay();
-        c4.ongoing = false;
-        if (clients.length >= 2) {
-            var player = ['Red', 'Yellow'];
-            io.to(clients[0]).emit('start', 'Playing as RED');
-            io.to(clients[1]).emit('start', 'Playing as YELLOW');
-            c4.ongoing = true;
-            io.emit('turn', player[c4.player]);
-            display();
-        } else {
-            io.emit('wait');
-        }
+        c4.newGame();
     }
+}
+
+function restartGame(status) {
+    status = status || '';
+    if (status) {
+        io.emit('status', status);
+    }
+    io.emit('turn', "Next game begins soon...");
+    var prev = clients.shift();
+    if (prev) {
+        clients.push(prev);
+    }
+    setTimeout(function() {
+        startGame();
+        display();
+    }, 10000);
+
 }
 
 function spectators() {
     for (var i = 2; i < clients.length; i += 1) {
-        io.to(clients[i]).emit('spectate');
+        io.to(clients[i]).emit('status', "Spectating...");
     }
 }
 
 function display() {
-    if (c4.ongoing) {
-        var player = ['Red', 'Yellow'];
-        var col_str = [];
-        for (var j = 0; j < c4.COLUMNS; j += 1) {
-            var tmp = [];
-            for (var i = 0; i < c4.ROWS; i += 1) {
-                var p = c4.getPiece(i, j);
-                if (p === -1) {
-                    break;
-                } else if (p === 0) {
-                    tmp.push('R');
-                } else if (p === 1) {
-                    tmp.push('Y');
-                }
-            }
-            col_str.push(tmp.join(''));
-        }
-
-        var display_msg = col_str.join('-');
-        io.emit('display', display_msg);
-        io.emit('turn', player[c4.player]);
+    if (c4.isWon()) {
+        var players = ['Red', 'Yellow'];
+        io.emit('status', players[c4.whoWon()] + " wins.");
+        io.emit('turn', "Next game begins soon...");
+        displayBoard();
+    } else if (c4.inGame()) {
+        io.to(clients[0]).emit('status', 'Playing as RED');
+        io.to(clients[1]).emit('status', 'Playing as YELLOW');
+        io.emit('turn', player[c4.player] + "'s turn.");
         spectators();
+        displayBoard();
+    } else {
+        io.emit('wait');
     }
+}
+
+function displayBoard() {
+    var col_str = [];
+    for (var j = 0; j < c4.COLUMNS; j += 1) {
+        var tmp = [];
+        for (var i = 0; i < c4.ROWS; i += 1) {
+            var p = c4.getPiece(i, j);
+            if (p === -1) {
+                break;
+            } else if (p === 0) {
+                tmp.push('R');
+            } else if (p === 1) {
+                tmp.push('Y');
+            }
+        }
+        col_str.push(tmp.join(''));
+    }
+    io.emit('display', col_str.join('-'));
 }
 
 function clearDisplay() {
