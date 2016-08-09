fa4a2c685cb9105ef293ab4eae2ff38e19d5caab
Joe Kuang <four@berkeley.edu>
2016-08-08 20:18:58 -0700

    Quick fixes. Code is convoluted; needs to be refactored.

--- c4.js
+++ c4.js
@@ -48,16 +48,25 @@ io.on("connection", function(socket) {
                 var col = parseInt(msg[0]);
                 c4.placePiece(col);
                 display();
+                var players = ['Red', 'Yellow'];
                 if (c4.isWon()) {
                     c4.ongoing = false;
-                    var players = ['Red', 'Yellow'];
                     io.emit('won', players[c4.whoWon()]);
+                    var prev = clients.shift();
+                    if (prev) {
+                        clients.push(prev);
+                    }
                     setTimeout(startNewGame, 10000);
+                    display();
                 } else if (c4.boardFull()) {
                     c4.ongoing = false;
-                    var players = ['Red', 'Yellow'];
                     io.emit('won', 'Neither player');
+                    var prev = clients.shift();
+                    if (prev) {
+                        clients.push(prev);
+                    }
                     setTimeout(startNewGame, 10000);
+                    display();
                 }
             }
         }
